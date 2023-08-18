292ab759c5cb72d74ef6fa53b8bfcd4b060542df
sameer515 <sameer515@hotmail.com>
2023-08-18 17:05:01 +0300

    v0.01

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
+const fs = require('fs');
 const express = require('express');
 
 const app=express();
 
+app.use(express.json());
+
 const port= 3000;
 
-app.get('/',(req,res)=>{
-res
-.status(200)
-.json({message :'Hello Sfrom the server',app: 'Natours'});
+const tours =JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+    );
+
+
+app.get('/api/v1/tours',(req, res) => {
+res.status(200).json ({
+    status: 'Success',
+    results:tours.length,
+    data: {tours}
+});
 });
 
-app.post('/',(req,res)=>{
+app.post('/api/v1/tours',(req, res) =>{
+   // console.log(req.body);
+   const newId = tours[tours.length -1].id +1 ;
+   const newTour = Object.assign({id: newId},req.body);
+   tours.push(newTour);
 
-    res.send('You can send post');
+   fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours)
+    ,error =>{
+    res.status(201).json({
+        status: 'Success',
+          data: {
+            tours: newTour
+          } 
+        });
+      }
+   );
 
-})
+   });
+   // res.send('Done');
+
 
 app.listen(port,()=>{
 
