726ade21166b224916ed843e47378f85f930b718
sameer515 <sameer515@hotmail.com>
2023-08-21 17:29:37 +0300

    modified:   app.js

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ res.status(200).json ({
 });
 });
 
+app.get('/api/v1/tours/:id',(req, res) => {
+
+  const id=req.params.id*1;
+  const tour=tours.find(el => el.id === id);
+
+ // if(id>tours.length){
+  if(!tour){
+    return res.status(404).json({
+        status: 'Error',
+        message: 'Tour not found or invalid ID'
+    });
+  }
+
+ 
+  res.status(200).json ({
+             data: {tour}
+  });
+  });
+
 app.post('/api/v1/tours',(req, res) =>{
    // console.log(req.body);
    const newId = tours[tours.length -1].id +1 ;
@@ -41,6 +60,45 @@ app.post('/api/v1/tours',(req, res) =>{
 
    });
    // res.send('Done');
+
+app.patch('/api/v1/tours/:id',(req,res)=>{
+
+  if(req.params.id *1 >tours.length)
+  {
+    return res.status(404).json({
+        status: 'Error',
+        message: 'Tour not found or invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status:'Success',
+    data : {
+      tours: 'update tour here'
+    }
+
+  });
+
+});
+
+
+app.delete('/api/v1/tours/:id',(req,res)=>{
+
+  if(req.params.id *1 >tours.length)
+  {
+    return res.status(404).json({
+        status: 'Error',
+        message: 'Tour not found or invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status:'Success',
+    data : null
+
+  });
+
+});
 
 
 app.listen(port,()=>{
