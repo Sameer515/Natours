92111f7a93fc3d77dc223e6ee79cf0891d1479ca
sameer515 <sameer515@hotmail.com>
2023-08-22 16:04:09 +0300

    v0.89

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,31 @@ const tours =JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
     );
 
+exports.checkID =(req,res,next,val) => {
+    if(req.params.id *1 >tours.length)
+    {
+      return res.status(404).json({
+          status: 'Error',
+          message: 'Tour not found or invalid ID'
+      });
+    }
+    next();
+};
+
+
+exports.checkBody =(req,res,next,) => {
+if(!req.body.name || !req.body.price)
+{
+    return res.status(404).json({
+        status:'fali',
+        message: 'missing name or price'
+
+})
+}
+next();
+
+};
+
 exports.getallTours= (req, res) => {
     res.status(200).json({
       status: 'Success',
@@ -17,14 +42,6 @@ exports.getallTours= (req, res) => {
     
       const id=req.params.id*1;
       const tour=tours.find(el => el.id === id);
-    
-     // if(id>tours.length){
-      if(!tour){
-        return res.status(404).json({
-            status: 'Error',
-            message: 'Tour not found or invalid ID'
-        });
-      }
     
      
       res.status(200).json ({
@@ -55,14 +72,6 @@ exports.getallTours= (req, res) => {
     
       exports.updateTour = (req,res)=>{
     
-        if(req.params.id *1 >tours.length)
-        {
-          return res.status(404).json({
-              status: 'Error',
-              message: 'Tour not found or invalid ID'
-          });
-        }
-      
         res.status(200).json({
           status:'Success',
           data : {
@@ -89,4 +98,5 @@ exports.getallTours= (req, res) => {
       
         });
       
-      }
+    }
+
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,11 +4,12 @@ const tourController=require('../controllers/tourController')
 
 const router = express.Router();
 
+router.param('id',tourController.checkID);
 
 router
 .route('/')
 .get(tourController.getallTours)
-.post(tourController.createTour);
+.post(tourController.checkBody,tourController.createTour);
 
 router
 .route('/:id')
