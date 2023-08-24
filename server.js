aaf353991b0828555f443fdf320bc4bbffa9c810
sameer515 <sameer515@hotmail.com>
2023-08-24 12:47:15 +0300

    v1 adding mongoo DB

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const dotenv = require('dotenv');
 
-dotenv.config({path:`./config.env`})
+dotenv.config({ path: `./config.env` });
 
-const app=require('./app')
+const app = require('./app');
 
+const port = process.env.PORT || 3000;
 
-
-const port= process.env.PORT || 3000;
-
-app.listen(port,()=>{
-
-    console.log(`Server is running on port ${port}`);
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
