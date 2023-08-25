45b5b0b707aee41efed3180e6262c9b10ca1cff2
sameer515 <sameer515@hotmail.com>
2023-08-25 15:46:15 +0300

    v1.01

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: `./config.env` });
+
+const DB = process.env.DataBase.replace(
+  '<PASSWORD>',
+  process.env.Database_Password,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection established'));
 
 const app = require('./app');
 
